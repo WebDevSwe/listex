395086a9ccd3f48b7a9be311ff3e642a11c9b3d1
WebDevSwe <tobias.wiklund@consid.se>
2020-09-21 23:27:15 +0200

    Lagt över så menyn skrivs ut med javascript

--- Assets/js/engine.js
+++ Assets/js/engine.js
@@ -2,20 +2,18 @@
 //https://github.com/WebDevSwe/listex
 
 //Global variables
-gMenuState = "start";
-gInfoText = "";
-gMenuText = "";
+var gMenuState = "start";
+var gInfoText = "";
+var gMenuText = "";
 
 //Element
-gContent = ".lis-content";
+var gContent = ".lis-content";
+var gMenu    = "#lis-main-nav"; 
+
 
 
 $(function() {
-    
-    gameEngine()
-
-    
-
+    gameEngine();
 
 });
 
@@ -23,23 +21,31 @@ function gameEngine(){
     if(gMenuState == "start"){
         gInfoText  = "<h1>Välkommen till ListEx</h1>";
         gInfoText += "<p>Ett program för att öva på glosor.</p>";
-        gMenuText  = "Vad vill du göra?;"
-
+        gMenuText  = "Vad vill du göra?"
+        
+        var menyList = [];
 
         $(gContent).html(gInfoText);
         $(gContent).append("<h2>" + gMenuText + "</h2>")
         //List menu. Text, id, class
-        $menyList[] = ("Öva på en lista",     "ux-button__start-exercise",  "start-exercise"  );
-        $menyList[] = ("Starta stort prov",   "ux-button__start-big-test",  "start-big-test"  );
-        $menyList[] = ("Skap en ny lista",    "ux-button__create-new-list", "create-new-list" );
-        $menyList[] = ("Hantera mina listor", "ux-button__manage-my-lists", "manage-my-lists" );
+        menyList[0] = ["Öva på en lista",     "start-exercise"  ];
+        menyList[1] = ["Starta stort prov",  "start-big-test"   ];
+        menyList[2] = ["Skap en ny lista",    "create-new-list" ];
+        menyList[3] = ["Hantera mina listor", "manage-my-lists" ];
 
 
-        
-
+       
       
 
 
     }
+
+    var listHtml = "";
+    var n;
+    for (n = 0; n < menyList.length; n++) {
+        listHtml += "<li><button id='ux-button__"+ menyList[n][1]+"'  class='lis-ux-button "+ menyList[n][1]+"'>" + menyList[n][0] + "</button></li>";
+        
+    }
+    $(gContent).append("<nav id='lis-main-nav' class='lis-main-nav'><ul>" + listHtml + "</ul></nav>");
 }
 
